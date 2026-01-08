c8e1fd3c3e28b7951ac1ecc76ca77cfe887b59fb
naveenchander30 <naveenmoxx@gmail.com>
2026-01-08 14:13:00 +0530

    Striver SDE - Arrays

--- 121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock.go
@@ -1,5 +1,17 @@
 package LeetCode
 
 func maxProfit(prices []int) int {
+	maxProfit := 0
+	minPrice := prices[0]
 
+	for _, num := range prices {
+		if num < minPrice {
+			minPrice = num
+		}
+		profit := num - minPrice
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+	return maxProfit
 }
