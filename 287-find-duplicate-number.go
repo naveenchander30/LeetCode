e4bbe553f6bef8da38f70d9c9a503469ebd75636
naveenchander30 <naveenmoxx@gmail.com>
2026-01-26 18:18:40 +0530

    Striver SDE - Arrays Part 2

---
+++ 287-find-duplicate-number.go
@@ -0,0 +1,22 @@
+package LeetCode
+
+func findDuplicate(nums []int) int {
+    slow := nums[0]
+	fast := nums[0]
+	
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+
+	fast = nums[0]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return slow
+
+}
--- 56-merge-intervals.go
+++ 56-merge-intervals.go
@@ -1,1 +1,20 @@
 package LeetCode
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	newIntervals := make([][]int, 0)
+	currentInterval := intervals[0]
+	for i := 0; i < len(intervals)-1; i++ {
+		if currentInterval[1] < intervals[i+1][0] {
+			newIntervals = append(newIntervals, currentInterval)
+			currentInterval = intervals[i+1]
+		} else {
+			currentInterval[1] = max(currentInterval[1], intervals[i+1][1])
+		}
+	}
+	newIntervals = append(newIntervals, currentInterval)
+	return newIntervals
+}
